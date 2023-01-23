2495e049ba178be68aa10cce3568eec445802c97
Eldar <initmyself@gmail.com>
2023-01-23 14:46:08 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int ticket = 500;                          //Цена билета
+        int spentOnTicket = 20;                   //Потраченное кол-во рублей для 1 бонусной мили
+        int bonusMiles = ticket / spentOnTicket;  // spentOnTicket; //Бонусные мили
+
+        System.out.println(bonusMiles);
+
     }
 }
